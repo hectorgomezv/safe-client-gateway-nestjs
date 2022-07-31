592a7002ee6a02b545dec0ac774fdf7c6d13025b
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-07-31 14:07:22 +0200

    Add ContractsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContractsController } from './contracts/contracts.controller';
 import { ContractsService } from './contracts/contracts.service';
+import { ContractsModule } from './contracts/contracts.module';
 
 @Module({
-  imports: [],
+  imports: [ContractsModule],
   controllers: [AppController, ContractsController],
   providers: [AppService, ContractsService],
 })
