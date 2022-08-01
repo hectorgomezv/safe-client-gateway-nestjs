edaa254c9a195a6964562407392c855614ed7651
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-01 18:07:48 +0200

    Remove boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ContractsModule } from './contracts/contracts.module';
 
 @Module({
   imports: [ContractsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
